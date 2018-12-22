03ee63affa53834c9a6d9264d364a47ceb8366e2
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-22 00:00:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ['yikes', 'one', 'too', 'many']
+  ['am', 'one', 'too', 'many']
 end
 
 def add_element_to_end_of_array(array, element)
@@ -24,6 +24,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   ["am"]
+  return array
 end
 
 def retrieve_first_element_from_array(array)
